8041ccea6bdc9be3e34434acbfe6002d196c959a
shiluzhang <szhang256@wisc.edu>
2019-05-09 23:28:56 -0500

    Update HiCReg_modeltrain_pred.sh

--- Scripts/LearnModelsPred/HiCReg_modeltrain_pred.sh
+++ Scripts/LearnModelsPred/HiCReg_modeltrain_pred.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-## 1. Train on train.txt and Predict on hard test set:
-./regForest -t train0.txt -o out/ -k1 -l10 -n20 -b prior_merge.txt -d test0.txt
+### 2.1 Training mode:
+#### Train on training data and Predict on test set:
+./regForest -t Examples/Data/Gm12878_chr17_WINDOW_train0.txt -o Examples/out/ -k1 -l10 -n20 -b Examples/Data/prior_window.txt -d Examples/Data/Gm12878_chr17_WINDOW_test0.txt
 
-## 2. Predict on test sets with Models trained:
-./regForest -t train0.txt -o out/ -k1 -l10 -n20 -b prior_merge.txt -d test0.txt -s out/model/regtree_node
+### 2.2 Prediction mode:
+#### Predict on test sets with Models trained:
 
+./regForest -t Examples/Data/Gm12878_chr17_WINDOW_train0.txt -o out/ -k1 -l10 -n20 -b Examples/Data/prior_window.txt -d Examples/Data/Gm12878_chr17_WINDOW_test0.txt -s Examples/out/regtree_node
